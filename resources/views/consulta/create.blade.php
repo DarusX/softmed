2c2bd49d1ceefebc42853a457ab0bec0812063c0
Fabián Montero <mntr_rdrgz@hotmail.com>
2018-06-27 21:41:35 -0500

    Laravel Collective removido

--- resources/views/consulta/create.blade.php
+++ resources/views/consulta/create.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app') @section('content')
 <div class="col-sm-12">
-    
-    <form action="{{route('consulta.store')}}" method="post">
-     {{ csrf_field()}}
+
+  <form action="{{route('consulta.store')}}" method="post">
+    {{ csrf_field()}}
 
     <legend>Crear Consulta para {{$paciente->nombreCompleto}}</legend>
-  
-  
-     
+
+
+
     <input type="hidden" name="paciente_id" value="{{$paciente->id}}">
 
     <div class="form-group {{$errors->has('fecha')? ' has-error':''}}">
       <label class="control-label" for="">Fecha</label>
       <input type="date" name="fecha" class="form-control" value="{{old('fecha')}}">
     </div>
-     
+
     <div class="form-group {{$errors->has('hora')? ' has-error':''}}">
       <label class="control-label" for="">Hora</label>
       <input type="time" name="hora" class="form-control" value="{{old('hora')}}">
@@ -22,7 +22,7 @@
     <div class="form-group">
       <button type="submit" class="btn btn-info">Agendar</button>
     </div>
-  
+
   </form>
 </div>
-@endsection
+@endsectionss
