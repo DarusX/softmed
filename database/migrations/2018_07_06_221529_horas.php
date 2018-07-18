15579b9fa5f8f1dae56c23de023cce8150d95c81
ceci-jimg <ceci.jimg@gmail.com>
2018-07-17 23:47:07 -0500

    Creacion de tabla y campos "horas"
    Se creo la tabla para el catalogo de horas.

--- database/migrations/2018_07_06_221529_horas.php
+++ database/migrations/2018_07_06_221529_horas.php
@@ -13,7 +13,10 @@ class Horas extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('hora', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('hora');
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class Horas extends Migration
      */
     public function down()
     {
-        //
+         Schema::dropIfExists('hora');
     }
 }
---
+++ database/seeds/hora.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class hora extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
