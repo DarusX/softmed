f6bbaf9f7e42b2f74ccfb70cc2d480d87172a20e
emma106 <practicas106@hotmail.com>
2018-07-27 10:47:50 -0500

    Notas del paciente

--- resources/views/expediente/index.blade.php
+++ resources/views/expediente/index.blade.php
@@ -9,58 +9,23 @@
             <!-- Page Header-->
             <header class="page-header">
                 <div class="container-fluid">
-                    <h2 class="no-margin-bottom">Expediente</h2>
+                    <h2 class="no-margin-bottom">Notas del paciente</h2>
                 </div>
             </header>
             <!-- Breadcrumb-->
-            <section class="tables">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card">
-                                <div class="card-header d-flex align-items-center">
-                                    <h3 class="h4">Expediente del paciente </h3>
-                                </div>
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>Fecha</th>
-                                                    <th>Hora</th>
-                                                    <th>Consulta</th>
-                                                    <th>Receta</th>
-                                                </tr>
-                                            </thead>
-                                    
-                                            <tbody>
-                                                @foreach($consultas as $consulta)
-                                                <tr>
-                                                    <td>{{ $consulta->fecha }}</td>
-                                                    <td>{{ $consulta->hora->hora }}</td>
-                                                    <td>
-                                                        <a href="{{ route('consulta.edit', $consulta)}}" class="btn btn-xs btn-primary">
-                                                            <i class="fas fa-pencil-alt"></i>
-                                                        </a>
-                                                        <a href="{{ route('consulta.destroy', $consulta)}}" class="btn btn-xs btn-danger">
-                                                            <i class="fas fa-times"></i>
-                                                        </a>
-                                                    </td>
-                                                    <td>
-                                                    <a href="{{ route('receta.edit', ['receta' => $consulta])}}" class="btn btn-xs btn-default"><i class="fas fa-eye"></i></a>                                                        </a>
-                                                    </td>
-                                                    @include('consulta.modal')
-                                                </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+           <section>
+           @foreach($consultas as $consulta)
+           <div class="col-lg-6">
+                  <div class="card">
+                    <div class="card-header d-flex align-items-center">
+                      <h3 class="h4">{{$consulta->fecha}}</h3>
                     </div>
+                    <div class="card-body">
+                      <p>{{$consulta->nota->nota}}</p>
+                    </div>
+                  </div>
                 </div>
+                @endforeach
             </section>
         </div>
     </div>
